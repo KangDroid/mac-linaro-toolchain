22844a34f781270de470ea2f0426eeb484f7c56a
KangDroid <hyunwoo286@gmail.com>
2015-06-13 13:14:33 +0100

    First Commit

---
+++ lib/gcc/x86_64-apple-darwin14.3.0/4.9.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --with-gmp=/usr/local --with-mpfr=/usr/local --with-mpc=/usr/local --with-isl=/usr/local --prefix=/Users/KangDroid/linaro_prefix --enable-graphite --disable-isl-version-check --disable-multilib --enable-languages=c,c++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "core2" } };
